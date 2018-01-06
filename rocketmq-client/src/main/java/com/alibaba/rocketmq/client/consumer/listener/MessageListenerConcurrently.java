19afd5b685d0685e8a16e09574a5e2611a6b9403
chengaojie <chengaojie@didichuxing.com>
2018-01-06 11:03:02 +0800

    MessageListenerConcurrently

--- rocketmq-client/src/main/java/com/alibaba/rocketmq/client/consumer/listener/MessageListenerConcurrently.java
+++ rocketmq-client/src/main/java/com/alibaba/rocketmq/client/consumer/listener/MessageListenerConcurrently.java
@@ -1,9 +1,24 @@
 package com.alibaba.rocketmq.client.consumer.listener;
 
+import com.alibaba.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+// done
 /**
- * @author chengaojie
- *         2018/1/5
+ * A MessageListenerConcurrently object is used to receive asynchronously delivered messages concurrently
  */
 public interface MessageListenerConcurrently extends MessageListener {
-    ConsumeConcurrentlyStatus consumeMessage();
+    /**
+     * It is not recommend to throw exception,rather than returning ConsumeConcurrentlyStatus.RECONSUME_LATER if consumption failure
+     *
+     * @param msgs
+     *         msgs.size() >= 1<br>
+     *         DefaultMQPushConsumer.consumeMessageBatchMaxSize=1，you can modify here
+     * @param context
+     *
+     * @return
+     */
+    ConsumeConcurrentlyStatus consumeMessage(final List<MessageExt> msgs,
+                                             final ConsumeConcurrentlyContext context);
 }
